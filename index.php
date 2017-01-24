2ad31d06e593d4332c08a2d6055096a1da5a156a
adhimeong <adhimeong@gmail.com>
2017-01-24 09:08:25 +0700

    nambah parf 2

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
 	<h1>Hello Github</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptatem veniam dolore itaque officia dolores dolorem, aliquid doloremque distinctio iure eius corporis nihil aut, sint, repellendus praesentium laborum in quas!</p>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, quis ipsam tempora optio commodi vero, non iure doloremque atque debitis nam, enim consequuntur, facere repellat magni natus possimus iste! Nemo!
+	</p>
 </body>
 </html>
