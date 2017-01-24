8fd121133f9a90b92b09822cf8fc9a9826567971
adhimeong <adhimeong@gmail.com>
2017-01-24 09:06:58 +0700

    nambah paragraf

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Document</title>
 </head>
 <body>
-	
 	<h1>Hello Github</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptatem veniam dolore itaque officia dolores dolorem, aliquid doloremque distinctio iure eius corporis nihil aut, sint, repellendus praesentium laborum in quas!</p>
 </body>
 </html>
